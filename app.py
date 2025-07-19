8349ca6fb7fca297d5ff8cbabddc0410dd5f7434
Daniel Chodos <danielchodos@mac.lan>
2025-07-19 09:44:39 -0400

    Add root endpoint to support Uptime Robot health checks

--- app.py
+++ app.py
@@ -56,3 +56,10 @@ def run_scraper_background():
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+
+
+
+@app.get("/")
+def root():
+    return {"status": "Tee Time API is live"}
+    
